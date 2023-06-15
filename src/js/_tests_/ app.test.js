53b9da177aa71957a1f95b4c73b4b40c17f33893
arseniy shalamov <arsenshalamov16@gmail.com>
2023-06-16 01:38:24 +0300

    NEW

--- src/js/_tests_/ app.test.js
+++ src/js/_tests_/ app.test.js
@@ -2,10 +2,29 @@ import Character from "../app";
 
 test('checking error - wrong type', () => {
     const char = () => new Character('Harry', 'Knight');
-    expext(char).toThrow('Странное имя.');
+    expext(char).toThrow('wrong type.');
 });
 
 test('checking error - wrong name', () => {
     const char = () => new Character('Fkjneirgnkjwvmker', 'Swordsman');
-    expext(char).toThrow('Стоит изменить.');
+    expext(char).toThrow('wrong name.');
+});
+
+test('checking set damage', () => {
+    const char = new Daemon('Marry', 'Daemon');
+    char.damage(12);
+    expect(char.health).toBe(92.8);
+});
+
+test('checking level up', () => {
+    const char = new Daemon('Marry', 'Daemon');
+    char.levelUp();
+    expect(char.level).toBe(2);
+});
+
+test('checking error - dead character', () => {
+    const chart = new Daemon('Marry', 'Daemon');
+    chart.health = 0;
+    const t = () => chart.levelUp();
+    expect(t).toThrow(new Error("Character's dead"));
 });
--- src/js/app.js
+++ src/js/app.js
@@ -5,14 +5,34 @@ let types = ['Bowerman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
 export default class Character {
     constructor(name, type) {
         if (name.length < 2 || name.length > 10) {
-            throw new Error('Странное имя.');
-        } else if (!types.includes(type)) {
-            throw new Error('Стоит изменить.')
-        } else {
-            this.name = name;
-            this.type = type;
+            throw new Error('wrong name.');
         }
+        
+        if (!types.includes(type)) {
+            throw new Error('wrong type.')
+        }
+
+        this.name = name;
+        this.type = type;
+        
         this.health = 100;
         this.level = 1;
+    }
+
+    levelUp() {
+        if (this.health == 0) {
+            throw new Error('нельзя повысить левел умершего');
+        } else {
+            this.level += 1;
+            this.attack *= 1.2;
+            this.defence *= 1.2;
+            this.health = 100;
+        }
+    }
+
+    damage(points) {
+        if (this.health >= 0) {
+            this.health = points * (1 - defence / 100);
+        }
     }
 }
